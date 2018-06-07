531a155c6943fdb1b5ba9b62468d94b48630a554
Stephen Myers <scmyers11@gmail.com>
2018-06-07 16:01:14 -0500

    Map models to models instead of model

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const CarQuery = require('./lib/car-query');
 module.exports = {
   getMakes: CarQuery.request.bind(null, 'getMakes', 'make'),
   getModel: CarQuery.request.bind(null, 'getModel', 'model'),
-  getModels: CarQuery.request.bind(null, 'getModels', 'model'),
+  getModels: CarQuery.request.bind(null, 'getModels', 'models'),
   getTrims: CarQuery.request.bind(null, 'getTrims', 'trim'),
   getYears: CarQuery.request.bind(null, 'getYears', 'year'),
 };
